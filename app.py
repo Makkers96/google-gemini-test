02399bc7d984301b198a3330f92bbf809c4f6e83
Makkers96 <Makkers96@gmail.com>
2023-10-04 16:06:54 -0400

    sessions

--- app.py
+++ app.py
@@ -2,21 +2,22 @@ from flask import Flask, render_template, request, session
 from main import run_llm
 
 app = Flask(__name__)
+app.secret_key = "thisisasupersecretkey124"
 
 @app.route("/", methods=["GET", "POST"])
 def google_test():
 
-    llm_response = ""
+    if 'llm_response' not in session:
+        session['llm_response'] = ""
 
     if request.method == 'POST':
         if 'prompt' in request.form:
-            prompt = request.form.get('prompt')
+            session['prompt'] = request.form.get('prompt')
 
-            global llm_response
-            llm_response = run_llm(prompt)
+            session['llm_response'] = run_llm(session['prompt'])
 
     return render_template("google_test.html",
-                           llm_response=llm_response,
+                           llm_response=session['llm_response'],
                            )
 
 if __name__ == "__main__":
